803e132a472ffbb3a39a7e5048f4f3c229ff5623
Lydia <lydiaratto.com>
2023-09-19 16:56:21 +0100

    updared methods and tests

--- challenges/04_skill_challenge/lib/diary_entry.py
+++ challenges/04_skill_challenge/lib/diary_entry.py
@@ -2,6 +2,7 @@ class DiaryEntry:
     def __init__(self, title, contents):
         self.title = title
         self.contents = contents
+        self.start_reading_chunk = 0
 
     def format(self):
         return f"Title: {self.title}, Content: {self.contents}"
@@ -13,16 +14,12 @@ class DiaryEntry:
         return int(self.count_words() / wpm)
 
     def reading_chunk(self, wpm, minutes):
-        # Parameters
-        #   wpm: an integer representing the number of words the user can read
-        #        per minute
-        #   minutes: an integer representing the number of minutes the user has
-        #            to read
-        # Returns:
-        #   string: a chunk of the contents that the user could read in the
-        #           given number of minutes
-        #
-        # If called again, `reading_chunk` should return the next chunk,
-        # skipping what has already been read, until the contents is fully read.
-        # The next call after that should restart from the beginning.
-        pass
+        n_words = wpm * minutes
+        word_list = self.contents.split()
+        if self.start_reading_chunk > len(word_list):
+            self.start_reading_chunk = 0
+        start = self.start_reading_chunk
+        end = start + n_words
+        text_to_read = word_list[start:end]
+        self.start_reading_chunk = end
+        return ' '.join(text_to_read)
--- challenges/04_skill_challenge/tests/test_diary_entry.py
+++ challenges/04_skill_challenge/tests/test_diary_entry.py
@@ -20,3 +20,18 @@ def test_zero_for_no_words_in_contents():
 def test_correct_reading_minutes_count():
     entry = DiaryEntry('Holiday', 'I had a great time in Scotland and got to explore the highlands.')
     assert entry.reading_time(3) == 4
+
+def test_reading_chunk_with_4_wpm_and_2_mins():
+    entry = DiaryEntry('Holiday', 'I had a great time in Scotland and got to explore the highlands.')
+    assert entry.reading_chunk(4, 2) == 'I had a great time in Scotland and'
+
+def test_reading_chunk_with_next_chunk():
+    entry = DiaryEntry('Holiday', 'I had a great time in Scotland and got to explore the highlands.')
+    entry.reading_chunk(4, 2)
+    assert entry.reading_chunk(4, 2) == 'got to explore the highlands.'
+
+def test_restart_reading_chunk_after_end():
+    entry = DiaryEntry('Holiday', 'I had a great time in Scotland and got to explore the highlands.')
+    entry.reading_chunk(4, 2)
+    entry.reading_chunk(4, 2)
+    assert entry.reading_chunk(4, 2) == 'I had a great time in Scotland and'
